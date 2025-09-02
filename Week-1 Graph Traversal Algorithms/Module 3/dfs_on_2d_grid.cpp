cd447f080995fc560969532654e47d68235fa8ce
Shihanul Islam Rian <shihanulriyan@gmail.com>
2025-09-02 15:13:40 +0600

    Add files via upload

---
+++ Week-1 Graph Traversal Algorithms/Module 3/dfs_on_2d_grid.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+char grid[105][105];
+bool vis[105][105];
+vector<pair<int,int>> d = {{-1,0},{1,0},{0,-1},{0,1}};
+
+void dfs( int si, int sj)
+{
+    cout << si << " " << sj << endl;
+    vis[si][sj];
+    for(int i=0;i<4;i++)
+    {
+        int ci , cj;
+        ci = si + d[i].first;
+        cj = sj + d[i].second;
+        cout << ci << " " << cj << endl;
+    }
+}
+int main()
+{
+    int n,m;
+    cin >> n >> m;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            cin >> grid[i][j];
+        }
+    }
+    int si,sj;
+    cin >> si >> sj;
+    dfs(si,sj);
+    return 0;
+}
